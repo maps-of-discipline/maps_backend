16b6cbe11d04b033c6a1a036ac6ac480505101a2
De333mond <vladb539@gmail.com>
2024-03-12 18:27:29 +0300

    fix db

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 SECRET_KEY = 'e1c6fe5819c4bc8857e2cbce963a7be5904692eba862d1a809f195710aff5cdc'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kd_user:KBS8xChDY!DaE&6C@kd.mospolytech.ru:3306/kd_sandbox'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kd_user:KBS8xChDY!DaE&6C@kd.mospolytech.ru:3306/kd_user_test'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 
