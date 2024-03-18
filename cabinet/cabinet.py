a63091373865eb89237e801122e59ece3f483c04
Daniil Kirillov <chunaixxx@gmail.com>
2024-03-18 07:17:23 +0300

    add save-topic

--- cabinet/cabinet.py
+++ cabinet/cabinet.py
@@ -1,6 +1,6 @@
-from models.maps import AupData, AupInfo
+from models.maps import db, AupData, AupInfo
 from models.cabinet import RPD, Topics
-from flask import Blueprint, make_response, jsonify
+from flask import Blueprint, make_response, jsonify, request
 from cabinet.utils.serialize import serialize
 
 from take_from_bd import (control_type_r)
@@ -23,11 +23,12 @@ def rpd():
 # TODO Обработать ошибки и ненаход данных
 @cabinet.route('/lessons/<string:aupCode>', methods=['GET'])
 def topics(aupCode):
+    # Добавить сюда поиск по id_unique_dicipline, т.к.
+    # сейчас возвращаются все темы по всем дисциплинам
     aup: AupInfo = AupInfo.query.filter(AupInfo.num_aup == aupCode).first()
 
     if not aup:
         return jsonify([])
-
 
     rpd: RPD = RPD.query.filter(RPD.id_aup == aup.id_aup).first()
 
@@ -35,6 +36,29 @@ def topics(aupCode):
     topics = serialize(topics)
 
     return jsonify(topics)
+
+@cabinet.route('/save-topic', methods=['POST'])
+def saveTopic():
+    data = request.get_json()
+    print(data)
+
+    if 'id' not in data:
+        return make_response('Отсутствует поле "id"', 401)
+    
+    if 'lesson' not in data:
+        return make_response('Отсутствует поле "lesson"', 401)
+    
+    topic = Topics.query.filter(Topics.id == data['id']).first()
+
+    topic.chapter = data['lesson']['chapter']
+    topic.topic = data['lesson']['topic']
+
+    db.session.add(topic)
+    db.session.commit()
+
+    res = serialize(topic)
+
+    return make_response(jsonify(res))
 
 # Получение всех нагрузок дисциплины
 @cabinet.route('/control_types/<string:id_rpd>', methods=['GET'])
--- cabinet/utils/serialize.py
+++ cabinet/utils/serialize.py
@@ -1,2 +1,5 @@
 def serialize(models):
-    return [r.to_dict() for r in models]
+    if isinstance(models, list):
+        return [r.to_dict() for r in models]
+    else:
+        return models.to_dict()
