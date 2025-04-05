205f2c9d67c16dc7d50b186ce73f4e2478c918d3
Ишкильдин Рамазан <r.f.ishkildin@stud.mospolytech.ru>
2025-04-05 20:43:31 +0300

    added class permissionGRPCService

--- grpc/grpc_manager.py
+++ grpc/grpc_manager.py
@@ -1,54 +1,50 @@
 import grpc
 from functools import lru_cache
+from typing import Dict
 from config import GRPC_URL
 
 from src.grpc.auth import auth_pb2_grpc
 from src.grpc.permissions import permissions_pb2_grpc
 
 class GrpcChannelManager:
-    #Менеджер gRPC каналов
-
+    """Менеджер gRPC каналов"""
+    
     def __init__(self):
         self.url = GRPC_URL
         self.channels: Dict[str, grpc.aio.Channel] = {}
-        self.stubs = {}
-
+        self.stubs: Dict[str, object] = {}  # Тип стаба может варьироваться
+        
         self._initialize_channels()
 
-    def _initilize_chanels(self):
+    def _initialize_channels(self) -> None:
         """Инициализация всех каналов"""
         config = {
             "auth": auth_pb2_grpc.AuthServiceStub,
             "permissions": permissions_pb2_grpc.PermissionServiceStub,
         }
         
-        for title, stub in config.items():
+        for title, stub_class in config.items():
             channel = grpc.aio.insecure_channel(self.url)
-
             self.channels[title] = channel
+            self.stubs[title] = stub_class(channel)
 
-            if title == "auth":
-                self.stubs[title] = stub(channel)
-            elif title == "permissions":
-                self.stubs[title] = stub(channel)
-
-
-    def get_auth_stub(self):
-        # Получить стаб для сервиса пользователей
+    def get_auth_stub(self) -> auth_pb2_grpc.AuthServiceStub:
+        """Получить стаб для сервиса аутентификации"""
         return self.stubs.get("auth")
 
-    def get_permissions_stub(self):
-        #Получить стаб для сервиса продуктов
+    def get_permissions_stub(self) -> permissions_pb2_grpc.PermissionServiceStub:
+        """Получить стаб для сервиса разрешений"""
         return self.stubs.get("permissions")
-    
+
 @lru_cache()
-def get_channel_manager():
+def get_channel_manager() -> GrpcChannelManager:
+    """Получить экземпляр менеджера каналов"""
     return GrpcChannelManager()
 
-
-def get_auth_service():
+def get_auth_service() -> auth_pb2_grpc.AuthServiceStub:
+    """Получить сервис аутентификации"""
     return get_channel_manager().get_auth_stub()
 
-
-def get_permissions_service():
+def get_permissions_service()  -> permissions_pb2_grpc.PermissionServiceStub:
+    """Получить сервис разрешений"""
     return get_channel_manager().get_permissions_stub()
--- grpc/permission_service.py
+++ grpc/permission_service.py
@@ -1,3 +1,4 @@
+from typing import List
 from grpc.permissions import permissions_pb2
 from grpc.dto.permissions import Permission, CreatePermission
 from grpc.grpc_manager import get_permissions_service
@@ -7,11 +8,58 @@ class permissionGRPCService:
     def __init__(self) -> None:
         self.stub = get_permissions_service()
 
-    def get_permission(self, service:str) -> list[Permission]:
-        pass
+    def get_permissions(self, service:str) -> List[Permission]:
+        request = permissions_pb2.GetPermissionsByServiceRequest(service_name=service)
+        response = self.stub.GetPermissionsByService(request) 
 
-    def update_permission(self, service_name: str, permission: str) -> Permission:
-        pass    
+        permissions: List[Permission] = [
+            Permission(
+                id= el.id,
+                title= el.title,
+                verbose_name= el.verbose_name,
+            )
+            for el in response
+        ]
+        return permissions
+        
 
-    def update_permission(self, service_name: str, permission: str) -> None:
-        pass
+    def create_permissions(self, service_name: str, permission: CreatePermission) -> Permission:
+        permission_create = permissions_pb2.CreatePermission(
+            title=permission.title,
+            verbose_name=permission.verbose_name,
+        )
+        
+        request = permissions_pb2.CreateServicePermissionRequest(
+            service_name= service_name,
+            permission=permission_create,
+        )
+
+        response = self.stub.CreateServicePermission(request)
+        response_permission = response.permission
+        
+        return Permission(
+            id= response_permission.id,
+            title= response_permission.title,
+            verbose_name= response_permission.verbose_name,
+        )
+
+    def update_permissions(self, service_name: str, permission: Permission) -> None:
+        permission_update = permissions_pb2.Permission(
+            id= permission.id,
+            title= permission.title,
+            verbose_name= permission.verbose_name,
+        )
+        
+        request = permissions_pb2.UpdateServicePermissionRequest(
+            service_name= service_name,
+            permission= permission_update,
+        )
+
+        response = self.stub.UpdateServicePermission(request)
+
+        return response
+    
+    def delete_permissions(self, permission_id: str) -> None:
+        request = permissions_pb2.DeleteServicePermissionRequest(id= permission_id)
+        
+        self.stub.DeleteServicePermission(request)
